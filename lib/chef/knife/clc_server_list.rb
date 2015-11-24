395e2b216a5f68ab9d6db4633b42f8b207c446fb
Aleksandr Kuntsevich <kuntsevichh@gmail.com>
2015-11-24 20:10:22 +0300

    Client: Add command for server list

--- lib/chef/knife/clc_server_list.rb
+++ lib/chef/knife/clc_server_list.rb
@@ -7,14 +7,72 @@ class Chef
 
       banner 'knife clc server list (options)'
 
+      option :clc_datacenter,
+        :long => '--datacenter VALUE',
+        :description => 'Short string representing the data center you are querying'
+
+      option :clc_all,
+        :long => '--all',
+        :boolean => true,
+        :default => false,
+        :description => 'The attribute to return a list of all servers from all datacenters'
+
       def run
         $stdout.sync = true
+        validate!
+        render
+      end
 
-        servers = connection.list_servers(name_args[0])
+      def collection
+        if config[:clc_datacenter]
+          connection.list_servers(config[:clc_datacenter])
+        elsif config[:clc_all]
+          datacenters = connection.list_datacenters
 
-        properties = %w(id name description groupId locationId osType status)
+          datacenters.map do |dc|
+            connection.list_servers(dc['id'])
+          end.flatten
+        end
+      end
 
-        puts Formatador.display_table(servers, properties)
+      def headers
+        {
+          'id' => 'ID',
+          'name' => 'Name',
+          'description' => 'Description',
+          'groupId' => 'Group ID',
+          'osType' => 'OS Type',
+          'status' => 'Status',
+          'locationId' => 'Location ID'
+        }
+      end
+
+      def fields
+        %w(id name description groupId locationId osType status)
+      end
+
+      def filters
+        {
+          'description' => proc { |desc| desc.empty? ? '-' : desc }
+        }
+      end
+
+      def render
+        output = Hirb::Helpers::AutoTable.render(collection,
+          :headers => headers,
+          :fields => fields,
+          :filters => filters,
+          :resize => false)
+
+        puts output
+      end
+
+      def validate!
+        if config[:clc_datacenter].nil? && !config[:clc_all]
+          errors << 'Datacenter ID is required'
+        end
+
+        check_for_errors!
       end
     end
   end
