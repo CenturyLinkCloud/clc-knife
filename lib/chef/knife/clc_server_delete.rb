172a6463ff52094d3a326e5516a76578cf570e67
Alexander Sologub <alexander.sologub@rightscale.com>
2015-11-29 17:45:53 +0300

    Knife: Initial version of create server command

--- lib/chef/knife/clc_server_delete.rb
+++ lib/chef/knife/clc_server_delete.rb
@@ -5,12 +5,19 @@ class Chef
     class ClcServerDelete < Knife
       include Knife::ClcBase
 
+      option :clc_wait,
+        :long => '--wait',
+        :description => 'Wait for operation completion',
+        :boolean => true,
+        :default => false
+
       banner 'knife clc server delete (options)'
 
       def run
         $stdout.sync = true
 
-        datacenter = connection.delete_server(name_args[0])
+        links = connection.delete_server(name_args[0])
+        connection.wait_for(links['operation']) { putc '.' } if config[:clc_wait]
       end
     end
   end
